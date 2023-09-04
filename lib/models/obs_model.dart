c60691441655dd79fa9e4a905f625b07a62bc3dd
Ed23gu <dewyn23@gmail.com>
2023-09-04 10:12:54 -0500

    ss

--- lib/models/obs_model.dart
+++ lib/models/obs_model.dart
@@ -1,10 +1,23 @@
-class DepartmentModel {
-  final int id;
+class ObsModel {
+  final int user_id;
   final String title;
+  final String create_at;
+  final String date;
+  final String horain;
 
-  DepartmentModel({required this.id, required this.title});
+  ObsModel(
+      {required this.user_id,
+      required this.title,
+      required this.create_at,
+      required this.date,
+      required this.horain});
 
-  factory DepartmentModel.fromJson(Map<String, dynamic> data) {
-    return DepartmentModel(id: data['id'], title: data['title']);
+  factory ObsModel.fromJson(Map<String, dynamic> data) {
+    return ObsModel(
+        user_id: data['user_id'],
+        title: data['title'],
+        create_at: data['create_at'],
+        date: data['date'],
+        horain: data['horain']);
   }
 }
