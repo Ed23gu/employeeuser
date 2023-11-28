5fd41a48411448de2ec3342259bf472c71431eec
Ed23gu <dewyn23@gmail.com>
2023-11-28 15:47:32 -0500

    ofi

---
+++ lib/error_page_asis.dart
@@ -0,0 +1,21 @@
+import 'package:employee_attendance/examples/value_notifier/connection_status_value_notifier.dart';
+import 'package:employee_attendance/screens/errores/error_page.dart';
+import 'package:employee_attendance/screens/splash_screen.dart';
+import 'package:employee_attendance/utils/check_internet_connection.dart';
+import 'package:flutter/material.dart';
+
+class ErrorPageAsis extends StatelessWidget {
+  const ErrorPageAsis();
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: ConnectionStatusValueNotifier(),
+      builder: (context, ConnectionStatus status, child) {
+        return status != ConnectionStatus.online
+            ? ErrorPage()
+            : const SplashScreen();
+      },
+    );
+  }
+}
